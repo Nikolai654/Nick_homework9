a7df52805c2c989dea8b84481f0203ce83357eba
«Nikolai» <>
2022-11-11 18:20:31 +0300

    first commit

---
+++ 3/Program.cs
@@ -0,0 +1,17 @@
+﻿int A(int m, int n)
+{
+    if (m == 0) return n + 1;
+    if (m != 0 && n == 0) return A(m - 1, 1);
+    if (m > 0 && n > 0) return A(m - 1, A(m, n - 1));
+    return A(m, n);
+}
+
+
+
+Console.Write("Введите число m: ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Введите число n: ");
+int n = int.Parse(Console.ReadLine());
+if (m >= 0 && n >= 0) Console.Write(A(m, n));
+else Console.Write("Введите неотрицательное число");
+Footer
